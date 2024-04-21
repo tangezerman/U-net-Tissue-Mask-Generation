03858aa20d2e24dd99184f1658c6aabb7f108a46
tangezerman <tangezerman@gmail.com>
2024-04-22 01:17:44 +0300

    edited for external test dataset

--- Code/Main.py
+++ Code/Main.py
@@ -1,47 +1,66 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-import tools
-import tensorflow as tf
+import csv
 import cv2
 import numpy as np
+import tensorflow as tf
+import tools
 
-model_path = r"C:\Users\tange\Vs Code\16th\Submission\Models\model__256_16_0"
-WSI_path = r"D:\AIN3007_project\train_valid_data\Breast1__he\TCGA-A1-A0SM-01Z-00-DX1.svs"
-real_mask_path = r"D:\AIN3007_project\train_valid_data\Breast1__he\TCGA-A1-A0SM-01Z-00-DX1.xml"
-export_path = r"C:\Users\tange\Vs Code\16th\Submission\export"
+# Suppress TensorFlow info and warning messages
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
+# Define paths and parameters
+model_path = r"D:\U-net-Tissue-Mask-Generation\Models\model__256_16_0"
+external_dataset_path = "Paths\external_data_test.csv"
+export_path = r"D:\U-net-Tissue-Mask-Generation\Export"
 patch_size = 256
 
-slide = tools.open_slide(WSI_path)
-
-level = 5 
-if level > slide.level_count : level = slide.level_count-1
-
-downscale = slide.level_downsamples[level]//1
-canvas = slide.level_dimensions[level]
-
-img_array = tools.generate_img_array(slide,level)
-patch_list = tools.slice(img_array)
-matrix_shape = len(img_array) // patch_size
-
+# Load the pre-trained model
 model = tf.keras.models.load_model(model_path)
 
-predicted_patches = model.predict(patch_list)
-predicted_patches = (predicted_patches > 0.5).astype(np.uint8) * 255
+# Read external dataset paths from CSV
+with open(external_dataset_path, 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        # Extract information from the CSV row
+        WSI_path = row['WSI_path']
+        # Provide the path to the XML file containing ground truth masks
+        real_mask_path = row['real_mask_path']
 
-original_array_shape = predicted_patches.shape
-reshaped_array = predicted_patches.reshape(7, 7, original_array_shape[1], original_array_shape[2], original_array_shape[3])
-predicted_mask = reshaped_array.swapaxes(1, 2).reshape((original_array_shape[1]*7, original_array_shape[2]*7, original_array_shape[3]))
+        # Process the WSI
+        slide = tools.open_slide(WSI_path)
+        level = 5  # Define the level for processing
+        if level > slide.level_count:
+            level = slide.level_count - 1
+        downscale = slide.level_downsamples[level] // 1
+        canvas = slide.level_dimensions[level]
+        img_array = tools.generate_img_array(slide, level)
+        patch_list = tools.slice(img_array)
+        matrix_shape = len(img_array) // patch_size
 
-predicted_mask = cv2.resize(predicted_mask, dsize=canvas, interpolation=cv2.INTER_NEAREST)
+        # Predict masks for the patches
+        predicted_patches = model.predict(patch_list)
+        predicted_patches = (predicted_patches > 0.5).astype(np.uint8) * 255
+        original_array_shape = predicted_patches.shape
+        reshaped_array = predicted_patches.reshape(
+            matrix_shape, matrix_shape, original_array_shape[1], original_array_shape[2], original_array_shape[3])
+        predicted_mask = reshaped_array.swapaxes(1, 2).reshape(
+            (original_array_shape[1]*matrix_shape, original_array_shape[2]*matrix_shape, original_array_shape[3]))
+        predicted_mask = cv2.resize(
+            predicted_mask, dsize=canvas, interpolation=cv2.INTER_NEAREST)
 
-try:
-    real_mask = tools.generate_mask(real_mask_path,downscale,canvas)
-    iou = tools.calculate_iou(real_mask,predicted_mask)
-    dice = tools.calculate_dice_coefficient(real_mask,predicted_mask)
-    print(f"IOU: {iou}")
-    print(f"Dice: {dice}")
-except:
-    print("No real mask")
+        # Calculate IOU and Dice coefficient if ground truth mask is available
+        try:
+            real_mask = tools.generate_mask(real_mask_path, downscale, canvas)
+            iou = tools.calculate_iou(real_mask, predicted_mask)
+            dice = tools.calculate_dice_coefficient(real_mask, predicted_mask)
+            print(f"For {WSI_path}:")
+            print(f"IOU: {iou}")
+            print(f"Dice: {dice}")
+        except Exception as e:
+            print(f"For {WSI_path}:")
+            print("Error:", e)
 
-cv2.imwrite("slice.png", predicted_mask)
+        # Write the predicted mask
+        output_mask_path = os.path.join(export_path, os.path.splitext(
+            os.path.basename(WSI_path))[0] + "_predicted_mask.png")
+        cv2.imwrite(output_mask_path, predicted_mask)
